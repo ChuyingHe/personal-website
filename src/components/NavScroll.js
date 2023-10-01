c50cd64aad17e6660cd8d896a768f6c2eba68657
ChuyingHe <heidihcy@outlook.com>
2023-10-01 17:38:48 +0200

    add languages

--- src/components/NavScroll.js
+++ src/components/NavScroll.js
@@ -4,31 +4,35 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import { ReactComponent as IconLanguage } from "../asset/icon_language.svg";
+
 import "./NavScroll.css";
 
 function NavScroll() {
   return (
-    <Navbar className="chuying-navbar" variant="dark" expand="lg">
-      <Container fluid>
-        {/* <Navbar.Brand className="navbar-brand-color" href="#">
+    <div>
+      <Navbar className="chuying-navbar" variant="dark" expand="lg" fixed="top">
+        <Container fluid>
+          {/* <Navbar.Brand className="navbar-brand-color" href="#">
           Chuying He's Website
         </Navbar.Brand> */}
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            {/* <Nav.Link href="#home">Home</Nav.Link> */}
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#skill">Skills</Nav.Link>
-            <Nav.Link href="#works">Works</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              {/* <Nav.Link href="#home">Home</Nav.Link> */}
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#skill">Skills</Nav.Link>
+              <Nav.Link href="#works">Works</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
 
-          <Button variant="outline-success">Language</Button>
-          {/* <Form className="d-flex">
+            {/* <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder="Search"
@@ -37,9 +41,23 @@ function NavScroll() {
             />
             <Button variant="outline-success">Search</Button>
           </Form> */}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+
+          <Dropdown size="sm" align="end">
+            <Dropdown.Toggle id="dropdown-basic">
+              <IconLanguage id="icon-language" />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">🇬🇧</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">🇨🇳</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">🇩🇪</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">🇪🇸</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
